8c568e25d8245443fb63a4cbe0c2473a792f22fb
chenglong <chenglong.1999@bytedance.com>
2023-02-14 22:32:44 +0800

    lab3: speed up syscall

--- kernel/proc.c
+++ kernel/proc.c
@@ -202,6 +202,15 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // map pid
+  struct usyscall *us =  (struct usyscall*)kalloc();
+  us -> pid = p -> pid;
+  if(mappages(pagetable, USYSCALL, PGSIZE,
+              (uint64)us, PTE_R | PTE_U) < 0) {
+      uvmfree(pagetable, 0);
+      return 0;
+  }
+
   return pagetable;
 }
 
@@ -212,6 +221,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
